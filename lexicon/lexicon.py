d55d44c2732790b4b17251a55fb56cb1e1ca46d7
JanGodar <2017sofin@gmail.com>
2024-08-02 13:32:33 +0300

    deploy 1

--- lexicon/lexicon.py
+++
@@ -1,5 +0,0 @@
-LEXICON_RU: dict[str, str] = {
-    '/start': 'Hello! I am echo-bot for demonstartion middleware\n',
-    'no_echo': 'That type updates is not supported',
-    'button_pressed': 'You are button!'
-}
